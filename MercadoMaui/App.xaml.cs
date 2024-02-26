71dd62a278dd55cb3d2760bf6076ab4fc3d8a2f9
Usuario <Usuario@CPD03-M04>
2024-02-26 10:55:46 -0300

    Adicionar arquivos de projeto.

---
+++ MercadoMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MercadoMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MercadoMaui/Helpers/SQLiteDataBaseHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MercadoMaui.Helpers
+{
+    internal class SQLiteDataBaseHelper
+    {
+    }
+}
